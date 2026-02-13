6b015f130a6f1f1c47e59b1c2ec38491ecb77a56
jherrmann-mtu <jherrman@mtu.edu>
2026-02-13 17:21:05 -0500

    Introduce error

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     
     public static void main(String[] args) {
         System.out.println("Hello from github actions");
+        System.out.println("this will cause an error -->")
     }
     
 }
