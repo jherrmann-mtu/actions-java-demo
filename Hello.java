1ec750964ab019a87639328a1cdb75cb907224ec
jherrmann-mtu <jherrman@mtu.edu>
2026-02-13 17:22:07 -0500

    Resolve error

--- Hello.java
+++ Hello.java
@@ -2,7 +2,6 @@ public class Hello {
     
     public static void main(String[] args) {
         System.out.println("Hello from github actions");
-        System.out.println("this will cause an error -->")
     }
     
 }
